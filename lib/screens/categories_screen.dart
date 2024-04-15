c0a0548c0f4b2a726a6fd1dcbe1698a2a4eb4f3c
dbl8005 <dbl8005@gmail.com>
2024-04-15 23:41:01 +0300

    added grid view to screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,6 +12,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick a Category'),
       ),
       body: GridView(
+        padding: EdgeInsets.all(12),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
@@ -17,12 +20,8 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: [
-          Text('1'),
-          Text('2'),
-          Text('3'),
-          Text('4'),
-          Text('5'),
-          Text('6'),
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
