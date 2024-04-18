9ce1cc95bc4586e528eddd36748630540851315c
dbl8005 <dbl8005@gmail.com>
2024-04-18 19:06:48 +0300

    starting out

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -4,39 +4,50 @@ import 'package:meals/widgets/meal_item.dart';
 import '../models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen(
-      {super.key,
-      required this.title,
-      required this.meals,
-      required this.selectedCategory});
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+    required String selectedCategory,
+  });
 
-  final String selectedCategory;
   final String title;
   final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemBuilder: (context, index) => Text(meals[index].title),
+    );
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Nothing Here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Try a different category!',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+          ],
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: GridView(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          for (var meal in meals.where(
-              (element) => element.categories.contains(selectedCategory)))
-            MealItem(
-              id: meal.id,
-              title: meal.title,
-              imageUrl: meal.imageUrl,
-            ),
-        ],
-      ),
+      body: content,
     );
   }
 }
