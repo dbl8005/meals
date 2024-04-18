603133088565e4b8dd7239ebb37c88161842f7a4
dbl8005 <dbl8005@gmail.com>
2024-04-18 20:37:31 +0300

    finished style of card

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +31,9 @@ class MealItem extends StatelessWidget {
         child: Stack(
           children: [
             FadeInImage(
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
             ),
@@ -48,7 +61,20 @@ class MealItem extends StatelessWidget {
                   const SizedBox(
                     height: 12,
                   ),
-                  const Row(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MealItemTrait(
+                          icon: Icons.schedule, label: '${meal.duration} min'),
+                      SizedBox(width: 12),
+                      MealItemTrait(icon: Icons.work, label: complexityText),
+                      SizedBox(width: 12),
+                      MealItemTrait(
+                        icon: Icons.attach_money,
+                        label: affordabilityText,
+                      )
+                    ],
+                  ),
                 ]),
               ),
             )
