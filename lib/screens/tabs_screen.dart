e0432823d77d3eee93366bce9bb80077ed233f38
dbl8005 <dbl8005@gmail.com>
2024-04-23 22:55:21 +0300

    working filters

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -100,6 +100,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     Widget activePage = CategoriesScreen(
       onToggleFavorite: _toggleMealFavoriteStatus,
+      availableMeals: availableMeals,
     );
 
     var activePageTitle = 'Categories';
