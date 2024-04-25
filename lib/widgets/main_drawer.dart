775153366cfe0bb41785aa84f1c8db36a8c6edfd
dbl8005 <dbl8005@gmail.com>
2024-04-25 16:20:42 +0300

    .

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals/main.dart';
 
 class MainDrawer extends StatefulWidget {
   MainDrawer({super.key, required this.onSelectScreen});
