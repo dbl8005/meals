dd1a393816c63dafc85f9172ae70515432934e6c
dbl8005 <dbl8005@gmail.com>
2024-04-25 16:20:15 +0300

    undo darktheme switch

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,76 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/main.dart';
 
-class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key, required this.onSelectScreen});
+class MainDrawer extends StatefulWidget {
+  MainDrawer({super.key, required this.onSelectScreen});
 
   final void Function(String identifier) onSelectScreen;
 
   @override
+  State<MainDrawer> createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
+  final isDarkModeProvider = StateProvider<bool>((ref) => false);
+
+  @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: [
-        DrawerHeader(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Theme.of(context).colorScheme.primaryContainer,
-              Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8),
-            ],
-          )),
-          child: Row(
-            children: [
-              Icon(
-                Icons.fastfood,
-                size: 48,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              const SizedBox(width: 20),
-              Text(
-                'Cooking Up!',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
-              )
-            ],
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Theme.of(context).colorScheme.primaryContainer,
+                Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8),
+              ],
+            )),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.fastfood,
+                  size: 48,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: 20),
+                Text(
+                  'Cooking Up!',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.primary),
+                )
+              ],
+            ),
           ),
-        ),
-        ListTile(
-          leading: Icon(
-            Icons.restaurant,
-            size: 26,
-            color: Theme.of(context).colorScheme.onBackground,
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Meals',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 24),
+            ),
+            onTap: () {
+              widget.onSelectScreen('meals');
+            },
           ),
-          title: Text(
-            'Meals',
-            style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-                fontSize: 24),
+          ListTile(
+            leading: Icon(
+              Icons.filter_list_alt,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Filters',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 24),
+            ),
+            onTap: () {
+              widget.onSelectScreen('filters');
+            },
           ),
-          onTap: () {
-            onSelectScreen('meals');
-          },
-        ),
-        ListTile(
-          leading: Icon(
-            Icons.filter_list_alt,
-            size: 26,
-            color: Theme.of(context).colorScheme.onBackground,
-          ),
-          title: Text(
-            'Filters',
-            style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-                fontSize: 24),
-          ),
-          onTap: () {
-            onSelectScreen('filters');
-          },
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
