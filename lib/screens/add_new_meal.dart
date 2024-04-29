11ee3f8bb6efbfda759e6bd1deb0b909ec45b666
dbl8005 <dbl8005@gmail.com>
2024-04-29 17:00:24 +0300

    skeleton for new meal adding

--- lib/screens/add_new_meal.dart
+++ lib/screens/add_new_meal.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/providers/meals_provider.dart';
 
 class AddNewMeal extends ConsumerStatefulWidget {
   const AddNewMeal({super.key});
@@ -9,8 +11,69 @@ class AddNewMeal extends ConsumerStatefulWidget {
 }
 
 class _AddNewMealState extends ConsumerState<AddNewMeal> {
+  late Meal newMeal;
+  final mealNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    mealNameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Meal'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: mealNameController,
+              decoration: const InputDecoration(
+                labelText: 'Meal Name',
+              ),
+              style:
+                  TextStyle(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            FloatingActionButton(
+              // When the user presses the button, show an alert dialog containing
+              // the text that the user has entered into the text field.
+              onPressed: () {
+                newMeal = Meal(
+                  id: DateTime.now().toString(),
+                  title: mealNameController.text,
+                  categories: [],
+                  ingredients: [],
+                  steps: [],
+                  complexity: Complexity.simple,
+                  affordability: Affordability.affordable,
+                  imageUrl: '',
+                  duration: 0,
+                  isGlutenFree: false,
+                  isLactoseFree: false,
+                  isVegan: false,
+                  isVegetarian: false,
+                );
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      // Retrieve the text that the user has entered by using the
+                      // TextEditingController.
+                      content: Text(newMeal.title),
+                    );
+                  },
+                );
+              },
+              tooltip: 'Show me the value!',
+              child: const Icon(Icons.text_fields),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
